5c1ddbcaa6fac857baf2bb2627ed405dd450eebe
kkh <gimgyugyu96@gmail.com>
2025-04-30 16:46:00 +0900

    쿼터뷰 완성 플레이어 걸어다니기뛰기

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float speed;
+    float hAxis;
+    float vAxis;
+    bool walkDown;
+
+    Vector3 moveVec;
+
+    Animator anim;
+
+    void Awake()
     {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        walkDown = Input.GetButton("Walk");
+
+
+        moveVec = new Vector3(hAxis,0,vAxis).normalized;
+        transform.position += moveVec * speed *(walkDown? 0.3f : 1f)* Time.deltaTime;
+
+        anim.SetBool("IsRun",moveVec!= Vector3.zero);
+        anim.SetBool("IsWalk",walkDown);
+
+        transform.LookAt(transform.position + moveVec);
+
     }
 }
