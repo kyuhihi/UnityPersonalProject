c4dc139012b50aea87ee83ccba2c81e1aa4222a5
kkh <gimgyugyu96@gmail.com>
2025-05-01 13:19:49 +0900

    아이템 프리팹 제작

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,15 +7,11 @@ public class Item : MonoBehaviour
     public ItemType type;
     public int value;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    const float m_fRotateSpeed = 20f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.up * m_fRotateSpeed * Time.deltaTime);
     }
 }
